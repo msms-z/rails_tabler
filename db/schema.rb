527fc1ba6c5c1ef3be9e1e3a86892b6b6b360c57
Msms-NJ <msmsruby@gmail.com>
2022-03-29 23:04:34 +0800

    add user name;  fix sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_26_141146) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_29_134622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "pgcrypto"
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_26_141146) do
     t.serial "sort", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["mobile"], name: "index_users_on_mobile", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
