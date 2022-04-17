faf8f85358b0d3eb17fb6d4ab97f8668f33be495
Msms-NJ <msmsruby@gmail.com>
2022-04-17 23:01:14 +0800

    fix verify code send times limit

--- app/models/verify_code.rb
+++ app/models/verify_code.rb
@@ -1,13 +1,26 @@
 class VerifyCode < ApplicationRecord
 
     validate :mobile_day_limit, on: :create
+    after_commit :increment_send_times, on: :create
 
     private
 
     def mobile_day_limit
+        if Rails.cache.exist?(current_cache_key, raw: true) && Rails.cache.read(current_cache_key, raw: true).to_i > 5
+            self.errors.add(:mobile, :mobile_send_limit)
+        end
+    end
 
-        Rails.cache.fetch(self.mobile)
+    def increment_send_times
+        if Rails.cache.exist?(current_cache_key, raw: true)
+            Rails.cache.increment(current_cache_key, 1, raw: true)
+        else
+            Rails.cache.write(current_cache_key, 1, raw: true, expires_in: 24.hours)
+        end
+    end
 
+    def current_cache_key
+        "verify_code:#{self.mobile}"
     end
 
 
